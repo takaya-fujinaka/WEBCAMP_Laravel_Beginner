36caf69e2f621cd07294057af14df83f3c9c8951
EC2 Default User <ec2-user@ip-172-31-35-70.ap-northeast-1.compute.internal>
2023-12-12 09:50:00 +0000

    タスクの編集を書いた

--- resources/views/task/edit.blade.php
+++ resources/views/task/edit.blade.php
@@ -15,12 +15,12 @@
             <form action="{{ route('edit_save', ['task_id' => $task->id]) }}" method="post">
                 @csrf
                 @method("PUT")
-                タスク名:<input name="name" value="{{ $task->name }}"><br>
-                期限:<input name="period" type="date" value="{{ $task->period }}"><br>
-                タスク詳細:<textarea name="detail">{{ $task->detail }}</textarea><br>
-                重要度:<label><input type="radio" name="priority" value="1" @if ($task->priority == 1) checked @endif>低い</label> /
-                     <label><input type="radio" name="priority" value="2" @if ($task->priority == 2) checked @endif>普通</label> /
-                     <label><input type="radio" name="priority" value="3" @if ($task->priority == 3) checked @endif>高い</label><br>
+                タスク名:<input name="name" value="{{ old('name') ?? $task->name }}"><br>
+                期限:<input name="period" type="date" value="{{ old('period') ?? $task->period }}"><br>
+                タスク詳細:<textarea name="detail">{{ old('detail') ?? $task->detail }}</textarea><br>
+                重要度:<label><input type="radio" name="priority" value="1" @if ((old('priority') ?? $task->priority) == 1) checked @endif>低い</label> /
+                     <label><input type="radio" name="priority" value="2" @if ((old('priority') ?? $task->priority) ==2) checked @endif>普通</label> /
+                     <label><input type="radio" name="priority" value="3" @if ((old('priority') ?? $task->priority) ==3) checked @endif>高い</label><br>
                 <button>タスクを編集する</button>     
             </form>
         
