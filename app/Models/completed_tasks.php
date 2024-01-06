8e99fd485aabe16b9dc072c8e78c7850aa963b3c
EC2 Default User <ec2-user@ip-172-31-35-70.ap-northeast-1.compute.internal>
2024-01-06 11:56:06 +0000

    質問

--- app/Models/completed_tasks.php
+++ app/Models/completed_tasks.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class completed_tasks extends Model
+class CompletedTask extends \App\Models\Task
 {
     use HasFactory;
     protected $guarded = ['id'];
